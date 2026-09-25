2ee1e4143ade663b912e5bceffe9b243132d9a1d
Mariano <marianofernandez98@outlook.com>
2026-09-25 15:06:23 -0300

    Mejorar header y tema de colores

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,27 +1,50 @@
-// src/theme/theme.js
+const primary = '#0d9488';
+
+function oscurecerColor(hex, porcentaje = 0.2) {
+    const valor = hex.replace('#', '');
+
+    const r = parseInt(valor.substring(0, 2), 16);
+    const g = parseInt(valor.substring(2, 4), 16);
+    const b = parseInt(valor.substring(4, 6), 16);
+
+    const factor = 1 - porcentaje;
+
+    const nuevoR = Math.round(r * factor);
+    const nuevoG = Math.round(g * factor);
+    const nuevoB = Math.round(b * factor);
+
+    return `#${nuevoR.toString(16).padStart(2, '0')}${nuevoG
+        .toString(16)
+        .padStart(2, '0')}${nuevoB.toString(16).padStart(2, '0')}`;
+}
 
 export const theme = {
     colors: {
-        primary: '#0d9488',       // El color principal (botones, links)
-        background: '#ffffff',    // Fondo general de la app
-        textDark: '#0f172a',      // Títulos y textos principales
-        textMuted: '#64748b',     // Subtítulos y textos secundarios
-        border: '#cbd5e1',        // Bordes de los inputs
-        inputBg: '#f8fafc',       // Fondo de los inputs
-        errorBg: '#fee2e2',       // Fondo rojo claro para alertas
-        errorText: '#ef4444',     // Texto rojo para errores
-        disabled: '#94a3b8',      // Botón deshabilitado
+        primary,
+        primaryDark: oscurecerColor(primary),
+
+        background: '#ffffff',
+        textDark: '#0f172a',
+        textMuted: '#64748b',
+        border: '#cbd5e1',
+        inputBg: '#f9fafad5',
+        errorBg: '#fee2e2',
+        errorText: '#ef4444',
+        disabled: '#94a3b8',
     },
+
     sizes: {
         title: 28,
         subtitle: 16,
         body: 14,
         input: 16,
     },
+
     spacing: {
         sm: 8,
         md: 16,
         lg: 24,
         xl: 32,
+        headerTop: 1,
     }
 };
